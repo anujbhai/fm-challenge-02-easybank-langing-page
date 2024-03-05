2736bae12772398aaa16dbc1362d3674f3826f44
Anuj Upadhyay <openmindedlearning@gmail.com>
2024-03-05 23:33:02 +0530

    fm-challenge02-easybank-setup-and-navbar - animated menu.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
-(function() {
-  console.log('Log from scr/js/script.js!')
+(function () {
+  const btnHamburger = document.querySelector("#btnHamburger")
+  const header = document.querySelector(".easy-header")
+  const body = document.querySelector("body")
+  const fadeElements = document.querySelectorAll(".has-fade")
+
+  btnHamburger.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (header.classList.contains("open")) {
+      body.classList.remove('noscroll')
+      header.classList.remove("open")
+      fadeElements.forEach((element) => {
+        element.classList.remove("fade-in")
+        element.classList.add("fade-out")
+      })
+    } else {
+      body.classList.add('noscroll')
+      header.classList.add("open")
+      fadeElements.forEach((element) => {
+        element.classList.remove("fade-out")
+        element.classList.add("fade-in")
+      })
+    }
+  })
 })()
